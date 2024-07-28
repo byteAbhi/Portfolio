409ee5540f7bd4b981e3f8cdf86913cea51c039d
byteAbhi <abhishekmukhiya56321@gmail.com>
2024-07-28 23:17:49 +0530

    remove nodemodules from env

--- functions/app.js
+++ functions/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require("express");
 const serverless = require("serverless-http");
-const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const ejs=require("ejs")
+const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", ejs);
+app.engine('ejs', require('ejs').__express);
 app.set("views", path.join(__dirname, "../views"));
 
 app.use(express.static(path.join(__dirname, "../public")));
